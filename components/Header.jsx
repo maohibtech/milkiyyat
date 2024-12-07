91d11b924f436ca60ca489871948faf208119e52
maohibtech <khanmuradhasan@icloud.com>
2024-12-07 12:44:41 +0600

    Header component create

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            this is header
+  return (
+    <>
+      <header>
+        <div className="container mx-auto py-6">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center space-x-12">
+              <div>
+                <Link href="/">Milkiyyat</Link>
+              </div>
+              <nav className="space-x-6">
+                <Link href="#">Personal</Link>
+                <Link href="#">Marchant</Link>
+              </nav>
+            </div>
+            <nav className="space-x-8">
+              <Link href="#">Help</Link>
+              <Link href="#">Pricing</Link>
+              <Link href="#">Login</Link>
+              <Link href="#">Resister</Link>
+            </nav>
+          </div>
         </div>
-    );
+      </header>
+    </>
+  );
 };
 
 export default Header;
